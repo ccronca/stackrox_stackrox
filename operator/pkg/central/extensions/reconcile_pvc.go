deef20504de77fc22243e99ee39e29eb233e0d12
Cong Du <c-du@users.noreply.github.com>
2024-08-08 12:43:37 -0700

    Update operator/pkg/central/extensions/reconcile_pvc.go
    Co-authored-by: Marcin Owsiany <porridge@redhat.com>

--- operator/pkg/central/extensions/reconcile_pvc.go
+++ operator/pkg/central/extensions/reconcile_pvc.go
@@ -131,7 +131,7 @@ func (r *reconcilePVCExtensionRun) Execute() error {
 	if r.centralObj.DeletionTimestamp != nil || r.persistence == nil {
 		return r.handleDelete()
 	}
-	// Starting from 4.6, we do not create or own PVCTargetCentral
+	// Starting from 4.1 we do not create new PVCs for central, and starting from 4.6, we also disown them.
 	if r.target == PVCTargetCentral {
 		return r.handleDelete()
 	}
