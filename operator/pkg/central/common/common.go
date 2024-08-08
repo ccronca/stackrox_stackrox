c7ab1463cc901ab49dd362796ab508233f6cd604
Cong Du <c-du@users.noreply.github.com>
2024-08-08 12:44:03 -0700

    Update operator/pkg/central/common/common.go
    Co-authored-by: Marcin Owsiany <porridge@redhat.com>

--- operator/pkg/central/common/common.go
+++ operator/pkg/central/common/common.go
@@ -2,6 +2,6 @@ package common
 
 const (
 	// CentralPVCObsoleteAnnotation represents Central PVC has been obsoleted.
-	// Only used for test
+	// Only used for test.
 	CentralPVCObsoleteAnnotation = "platform.stackrox.io/obsolete-central-pvc"
 )
