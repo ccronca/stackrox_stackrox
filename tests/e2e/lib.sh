e4e0febb0e5a1305b37c71fa8df92582954c6aa5
Michaël Petrov <smartptr@gmail.com>
2024-08-14 21:22:12 +0200

    style

--- tests/e2e/lib.sh
+++ tests/e2e/lib.sh
@@ -23,7 +23,7 @@ envsubst=$(command -v envsubst)
 stacktrace() { 
    local i=1 line file func
    while read -r line func file < <(caller $i); do
-      echo >&2 "[$i] $file:$line $func(): $(sed -n ${line}p $file)"
+      echo >&2 "[$i] $file:$line $func(): $(sed -n "${line}p" "$file")"
       ((i++))
    done
 }
